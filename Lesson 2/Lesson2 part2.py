6ad821190cb56a1ab4b1e5fb9dd9de87dced4258
Studenti <blertahajdini5@gmail.com>
2024-11-02 12:39:30 +0100

    ..

---
+++ Lesson 2/Lesson2 part2.py
@@ -0,0 +1,25 @@
+contact_information = {
+    #Blerta's info in the dictionary
+
+    "Blerta": {
+        "phone_number": "555-3333",
+        "email": "blertahajdini5@gmail.com",
+        "birthday": "27/06/2000"
+    },
+
+    "Erosi": {
+        "phone_number": "333-444",
+        "email": "eroskrasniqi02@gmail.com",
+        "birthday":"31.01.2008"
+    },
+    "Driloni": {
+        "phone_number": "888-2222",
+        "email": "drilonlatifi9@gmail.com",
+        "birthday": "04.03.2008"
+    },
+
+
+}
+
+print(contact_information)
+
